4240fe7693249cc2537c71c95cc59264d8bbeed8
Saarth Rajan <149202885+SaarthRajan@users.noreply.github.com>
2025-05-07 17:47:40 -0400

    Test for OpenData API from UW

--- watclassroom-backend/server.py
+++ watclassroom-backend/server.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+uwaterloo_api = os.getenv("UWATERLOO_API_KEY")
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/locations/{location}")
-async def get_nearest_location(location):
+async def get_nearest_location(location: str):
     return location
+
+@app.get("/locations")
+async def get_all_locations():
+
+    url = "https://openapi.data.uwaterloo.ca/v3/Locations"
+
+    payload = {}
+    headers = {
+    'x-api-key': uwaterloo_api,
+    'accept': 'application/json'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return (response.text)
