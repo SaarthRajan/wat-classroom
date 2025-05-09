3521135325b795ec892b3396b78ec00b6b57bd69
Saarth Rajan <149202885+SaarthRajan@users.noreply.github.com>
2025-05-09 19:03:25 -0400

    Added nearest location api

--- watclassroom-backend/server.py
+++ watclassroom-backend/server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import requests
 import os
+from geopy.distance import geodesic as GD
 
 load_dotenv()
 uwaterloo_api = os.getenv("UWATERLOO_API_KEY")
@@ -16,6 +17,43 @@ async def root():
 async def get_nearest_location(location: str):
     return location
 
+@app.get("/find_nearest_locations/{building_code}")
+async def find_nearest_locations(building_code):
+
+    # must ensure that the building_code is correct
+    # ensure that with a checkbox
+
+    payload = {}
+    headers = {
+        'x-api-key': uwaterloo_api,
+        'accept': 'application/json'
+    }
+
+    url_location_code = f"https://openapi.data.uwaterloo.ca/v3/Locations/{building_code}"
+
+    url_locations = "https://openapi.data.uwaterloo.ca/v3/Locations"
+
+    
+    cur_loc_response = requests.request("GET", url_location_code, headers=headers, data=payload)
+
+    cur_loc_json = cur_loc_response.json()
+
+    cur_loc = (cur_loc_json["latitude"], cur_loc_json["longitude"])
+    
+    loc_response = requests.request("GET", url_locations, headers=headers, data=payload)
+
+    loc_json = loc_response.json()
+
+    distances = {}
+    
+    for entry in loc_json:
+        loc = entry["latitude"], entry["longitude"]
+        distances[entry["buildingCode"]] = f"{GD(loc,cur_loc).m} m"
+
+    sorted_dict = dict(sorted(distances.items(), key=lambda item: item[1]))
+
+    return sorted_dict
+
 @app.get("/locations")
 async def get_all_locations():
 
@@ -29,4 +67,11 @@ async def get_all_locations():
 
     response = requests.request("GET", url, headers=headers, data=payload)
 
-    return (response.text)
+    building_list = {}
+
+    response_json = response.json()
+
+    for entry in response_json:
+        building_list[entry["buildingCode"]] = entry["buildingName"]
+
+    return building_list
