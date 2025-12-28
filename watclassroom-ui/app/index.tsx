b67cfbc3b85a9bd862b2b32962aa114c4c8d7bb4
Saarth Rajan <149202885+SaarthRajan@users.noreply.github.com>
2025-12-27 22:31:31 -0500

    Added Branding

--- watclassroom-ui/app/index.tsx
+++ watclassroom-ui/app/index.tsx
@@ -432,6 +432,11 @@ function Index() {
             />
           )}
         </ScrollView>
+        <View className="items-center py-4">
+          <Text className="bg-[#cccccc] text-black px-4 py-2 rounded-3xl font-body">
+            Made with ❤️ by Saarth
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
